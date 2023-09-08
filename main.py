1a6a8f1f58f11bc506f42d3b47dca16a40e0208b
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-09-08 19:17:01 +0200

    Copy from dev
    Copy from dev

---
+++ main.py
@@ -0,0 +1,24 @@
+from phonebook.phonebook import main as phonebook_main
+
+
+def main():
+    while True:
+        print("1. Адресна книга")
+        print("2. Сортування")
+        print("3. Нотатки")
+        print("4. Вийти")
+
+        choice = input("Виберіть дію: ")
+
+        if choice == '1':
+            # Виклик функціональності з phonebook.py
+            phonebook_main()
+
+        # інші варіанти дій тут...
+
+        elif choice == '4':
+            break
+
+
+if __name__ == "__main__":
+    main()
