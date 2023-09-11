c238719a5bf5d72ba8cd6d5f92d6d716bb6da4e1
Filip-Povidernyi <p.poviderniy@gmail.com>
2023-09-11 09:49:28 +0300

    fixed edit_note
    Прибрав змінну note
    Поправив код

--- calc/evil_calculator.py
+++ calc/evil_calculator.py
@@ -1,64 +1,71 @@
-result = None
-operand = None
-operator = None
-wait_for_number = True
+def main():
 
-while True:
-    if wait_for_number:
+    result = None
+    operand = None
+    operator = None
+    wait_for_number = True
 
-        while True:
-            operand = input()
+    while True:
+        if wait_for_number:
 
-            try:
-                operand = float(operand)
-            except ValueError:
+            while True:
+                operand = input(">>> ")
 
-                print(f"{operand} is not a number. Try again.")
-                continue
+                try:
+                    operand = float(operand)
+                except ValueError:
 
-            else:
-                if operator == '/':
+                    print(f"{operand} is not a number. Try again.")
+                    continue
 
-                    try:
-                        result /= operand
+                else:
+                    if operator == '/':
 
-                    except ZeroDivisionError:
-                        print('Can\'t divide by Zero')
-                        continue
+                        try:
+                            result /= operand
 
-                elif operator == '*':
-                    result *= operand
+                        except ZeroDivisionError:
+                            print('Can\'t divide by Zero')
+                            continue
 
-                elif operator == '+':
-                    result += operand
+                    elif operator == '*':
+                        result *= operand
 
-                elif operator == '-':
-                    result -= operand
-            wait_for_number = False
-            break
+                    elif operator == '+':
+                        result += operand
 
-    elif operator == '=' and wait_for_number == False:
-        print(f'Result: {result}')
-        break
-
-    elif not operator:
-        result = operand
-        operator = True
-        continue
-
-    elif not wait_for_number:
-
-        while True:
-            operator = input()
-
-            if operator == '=' and result:
+                    elif operator == '-':
+                        result -= operand
                 wait_for_number = False
                 break
 
-            elif not (operator == '+' or operator == '/' or operator == '*' or operator == '-'):
-                print(f"'{operator}' is not '+' or '-' or '/' or '*'. Try again.")
-                continue
+        elif operator == '=' and wait_for_number == False:
+            print(f'Result: {result}')
+            break
 
-            else:
-                wait_for_number = True
-                break
+        elif not operator:
+            result = operand
+            operator = True
+            continue
+
+        elif not wait_for_number:
+
+            while True:
+                operator = input()
+
+                if operator == '=' and result:
+                    wait_for_number = False
+                    break
+
+                elif not (operator == '+' or operator == '/' or operator == '*' or operator == '-'):
+                    print(
+                        f"'{operator}' is not '+' or '-' or '/' or '*'. Try again.")
+                    continue
+
+                else:
+                    wait_for_number = True
+                    break
+
+
+if __name__ == "__main__":
+    main()
