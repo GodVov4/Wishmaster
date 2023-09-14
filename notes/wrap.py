61d114e5632a4819189aaf7614731a73a1a8bd5e
Filip-Povidernyi <p.poviderniy@gmail.com>
2023-09-14 17:39:58 +0300

    Update wrap.py
    Пофіксив вихід.
    Якщо створених нотаток немає, то просто виходить без зберігання пустого файлу

--- notes/wrap.py
+++ notes/wrap.py
@@ -199,17 +199,21 @@ def main():
         elif choice == "9":
 
             # Додаємо пропозицію повернутися у цикл вибору
-            result = input("Продовжити (y) чи повернутися у меню вибору (n)? ")
+            result = input("Вийти (y) чи повернутися у меню вибору (n)? ")
             if result == "n":
                 continue
 
             else:
 
-                # Зберігаємо список нотаток у файл
-                notebook.save_to_file()
-                print("Нотатки збережено")
+                if len(notebook.notes):
 
-                break
+                    # Зберігаємо список нотаток у файл
+                    notebook.save_to_file()
+                    print("Нотатки збережено")
+                    break
+
+                else:
+                    break
 
         else:
             print("Невірний вибір")
